61c0a44dbf9108600ca05c3c2c3baf7ac1333a16
dxps <marius.ileana@gmail.com>
2021-11-07 18:02:33 +0200

    refactor: modules per features

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,3 @@
-use std::net::TcpListener;
-
-use actix_web::dev::Server;
-use actix_web::{web, App, HttpResponse, HttpServer};
-
-#[derive(serde::Deserialize)]
-struct FormData {
-    email: String,
-    name: String,
-}
-
-fn index(form: web::Form<FormData>) -> String {
-    format!("Welcome {}", form.name)
-}
-
-async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-            .route("/subscriptions", web::post().to(subscribe))
-    })
-    .listen(listener)?
-    .run();
-    Ok(server)
-}
+mod config;
+mod routes;
+pub mod startup;
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::net::TcpListener;
 
-use rs_zero2prod::run;
+use rs_zero2prod::startup;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let listener =
         TcpListener::bind("127.0.0.1:8000").expect("Failed to listen to standard port (8000).");
 
-    run(listener)?.await
+    startup::run(listener)?.await
 }
